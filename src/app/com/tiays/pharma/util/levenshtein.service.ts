a396a88bcd5381f758aa9d6eda6d69cd6f3640ad
ekougs <ekougs@gmail.com>
2016-05-01 12:19:59 +0000

    Correct typos ; styling adjustment

--- src/app/com/tiays/pharma/util/levenshtein.service.ts
+++ src/app/com/tiays/pharma/util/levenshtein.service.ts
@@ -1,4 +1,4 @@
-///<reference path="../declar/levenshtein/index.d.ts" />
+///<reference path="../declare/levenshtein/index.d.ts" />
 
 import {Injectable,Inject} from "angular2/core";
 import levenshtein = require('levenshtein');
